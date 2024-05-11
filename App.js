ba48163b6718ebfdad2cfb212e6801e263ab3066
Levent Elçiçek <96915201+lewo56657@users.noreply.github.com>
2024-05-11 16:44:30 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,80 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import FoodOverviewScreen from './screens/FoodOverviewScreen';
-import FoodDetailScreen from './screens/FoodDetailScreen';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import FavoritesScreen from './screens/FavoritesScreen';
-import { Ionicons } from '@expo/vector-icons';
-import FavoritesContextProvider from './store/favoritescontext';
-import { Provider } from 'react-redux';
-import { store } from './store/redux/store';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
-const Drawer = createDrawerNavigator();
-
-function DrawerNavigator() {
-  return (
-    <Drawer.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: 'blue' },
-        headerTintColor: 'white',
-      }}
-    >
-      <Drawer.Screen
-        name="Categories"
-        component={CategoriesScreen}
-        options={{
-          title: 'Tüm Kategoriler',
-          drawerIcon: () => <Ionicons name="list" size={24} color="black" />,
-        }}
-      />
-      <Drawer.Screen
-        name="Favorites"
-        component={FavoritesScreen}
-        options={{
-          title: 'Favoriler',
-          drawerIcon: () => (
-            <Ionicons name="ios-star-half" size={24} color="black" />
-          ),
-        }}
-      />
-    </Drawer.Navigator>
-  );
-}
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* <FavoritesContextProvider> */}
-      <Provider store={store}>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: 'blue' },
-            headerTintColor: 'white',
-            contentStyle: { backgroundColor: 'lightblue' },
-          }}
-        >
-          <Stack.Screen
-            name="Drawer"
-            component={DrawerNavigator}
-            options={{
-              headerShown: false,
-            }}
-          />
-          <Stack.Screen name="FoodOverview" component={FoodOverviewScreen} />
-          <Stack.Screen
-            name="FoodDetail"
-            component={FoodDetailScreen}
-            options={{
-              title: 'İçerik',
-            }}
-          />
-        </Stack.Navigator>
-      </Provider>
-      {/* </FavoritesContextProvider> */}
+      <Stack.Navigator>
+        <Stack.Screen
+          options={{ headerShown: false }}
+          name="Login"
+          component={LoginScreen}
+        />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
